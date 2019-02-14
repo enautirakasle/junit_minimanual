e78d00960124480f1b8a7172da4538e5e7926418
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-02-14 22:47:24 +0100

    beforeClass afterClass

--- src/calculadora/CalculadoraTest.java
+++ src/calculadora/CalculadoraTest.java
@@ -3,28 +3,40 @@ package calculadora;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculadoraTest {
 
-	Calculadora calc;
+	static Calculadora calc;
+	
+	@BeforeClass
+	public static void beforeClass(){
+		System.out.println("beforeClass()");
+		calc = new Calculadora();
+	}
 	
 	@Before
 	public void before(){
-		System.out.println("Before");
-		calc = new Calculadora();
+		System.out.println("before()");
+	}
+	
+	@AfterClass
+	public static void afterClass(){
+		System.out.println("afterClass()");
 	}
 	
 	@After
 	public void after(){
-		System.out.println("After");
+		System.out.println("after()");
 		calc.clear();
 	}
 	
 	@Test
 	public void testSum(){
-		System.out.println("testSum");
+		System.out.println("testSum()");
 		
 //		int actual = calc.add(3,2);
 //		int expected = 5;
@@ -34,7 +46,7 @@ public class CalculadoraTest {
 	
 	@Test
 	public void testAnsSum(){
-		System.out.println("testAnsSum");
+		System.out.println("testAnsSum()");
 		calc.add(3,2);
 		int actual = calc.ans();
 		int expected = 5;
@@ -44,16 +56,19 @@ public class CalculadoraTest {
 	
 	@Test
 	public void testDiv(){
+		System.out.println("testDiv()");
 		calc.div(5, 2);
 	}
 	
 	@Test(expected = ArithmeticException.class)
 	public void testDivPorCero(){
+		System.out.println("testDivPorCero()");
 		calc.div(5, 0);
 	}
 	
-	@Test(timeout = 100)
+	@Test(timeout = 3000)
 	public void testOperacionOptima(){
+		System.out.println("testOperacionOptima()");
 		calc.operacionOptima();
 	}
 
