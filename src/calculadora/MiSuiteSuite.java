4ed4d115736630a5fb7ddf5842e9b7d863db81b0
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-02-15 10:03:47 +0100

    suite

---
+++ src/calculadora/MiSuiteSuite.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value = Suite.class)
+@Suite.SuiteClasses({
+	CalculadoraTest.class,
+	CalculadoraParametroTest.class
+})
+public class MiSuiteSuite {
+
+}
